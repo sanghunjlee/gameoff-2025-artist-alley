2e6c8b070d6ccdcb497d4f622c8215fa7412921b
sabrina <sabriy2@uci.edu>
2025-11-11 21:34:34 -0800

    added bubble
    booble

---
+++ game/ui/hud/action_buttons.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _on_get_inspiration_button_pressed():
+    GameState.inspiration_point += randi_range(1, 5)
+
+func _on_make_merch_button_pressed():
+    pass # Replace with function body.
+
+func _on_sleep_button_pressed() -> void:
+    GameState.player.do_task(GameState.player.Task.SLEEP)
+
+func _on_tv_button_pressed() -> void:
+    GameState.player.do_task(GameState.player.Task.WATCH_TV)
+
+func _on_pc_button_pressed() -> void:
+    GameState.player.do_task(GameState.player.Task.USE_PC)
+
+func _on_draw_button_pressed() -> void:
+    GameState.player.do_task(GameState.player.Task.DRAW)
+    pass # Replace with function body.
