288763e460d7762f4ab9c93336307aeba5a9d1d7
sabrina <sabriy2@uci.edu>
2025-11-13 21:18:10 -0800

    fix spacing between characters in mini speech bubble

--- game/scripts/autoload/message_manager.gd
+++ game/scripts/autoload/message_manager.gd
@@ -1,10 +1,10 @@
 extends Node
 # Uses DialogueManager
-@onready var my_resource = preload("res://dialogue/my_dialogue.dialogue")
+# @onready var my_resource = preload("res://dialogue/my_dialogue.dialogue")
 
-func _ready() -> void:
-    print_debug("MessageManager ready, starting dialogue...")
-    start_dialogue()
+# func _ready() -> void:
+#     print_debug("MessageManager ready, starting dialogue...")
+#     start_dialogue()
 
-func start_dialogue() -> void:
-    DialogueManager.show_dialogue_balloon(my_resource, "start")
+# func start_dialogue() -> void:
+#     DialogueManager.show_dialogue_balloon(my_resource, "start")
