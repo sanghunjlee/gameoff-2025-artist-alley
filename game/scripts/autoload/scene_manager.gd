ca496ca61f9648aad298446398af6985f689d8f7
sabrina <sabriy2@uci.edu>
2025-11-30 12:03:13 -0800

    moved scenes_data to gamestate.gd

--- game/scripts/autoload/scene_manager.gd
+++ game/scripts/autoload/scene_manager.gd
@@ -1,8 +1,6 @@
 extends Node
 # manages changing scenes
-var scenes_data: ScenesData = preload("res://game/resources/scenes/ScenesData.tres")
-var main_scene = scenes_data.main_scene
-var convention_scene = scenes_data.convention_scene
 
 func change_scene_to(scene: PackedScene) -> void:
+    GameState.current_scene = scene
     get_tree().change_scene_to_packed(scene)
