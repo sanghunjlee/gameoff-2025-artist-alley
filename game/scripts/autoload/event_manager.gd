799d8b8c0879c22a4a4ab6c69ccb223c41dc6aed
sabrina <sabriy2@uci.edu>
2025-11-30 13:20:01 -0800

    fixed bug where con didnt start on day 14

--- game/scripts/autoload/event_manager.gd
+++ game/scripts/autoload/event_manager.gd
@@ -1,18 +1,15 @@
 extends Node
 
-var con_happened: bool = false
-
 func _ready() -> void:
     TimeManager.connect("time_updated", Callable(self, "_on_time_updated"))
     StatsManager.connect("money_depleted", Callable(self, "go_to_game_over"))
 
 func _on_time_updated() -> void:
-    if TimeManager.get_current_day() % 7 == 0 and !con_happened:
+    if TimeManager.get_current_day() % 7 == 0 and GameState.current_scene == GameState.scenes_data.main_scene:
         go_to_convention()
 
 func go_to_convention() -> void:
     SceneManager.change_scene_to(GameState.scenes_data.convention_scene)
-    con_happened = true
 
 func go_to_game_over() -> void:
     SceneManager.change_scene_to(GameState.scenes_data.game_over_scene)
