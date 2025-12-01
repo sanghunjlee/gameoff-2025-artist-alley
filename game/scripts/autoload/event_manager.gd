4f683967c8b0190619c09b74d6f17103493b653d
Joseph Lee <sanghunjlee@gmail.com>
2025-12-01 01:08:29 -0800

    Fix: Corrupt landlord keep taking money till bankrupcy

--- game/scripts/autoload/event_manager.gd
+++ game/scripts/autoload/event_manager.gd
@@ -1,19 +1,24 @@
 extends Node
 
+var is_rent_collected: bool = false
 var rent_due_dialogue = preload("res://game/dialogue/rent_due_dialogue.dialogue")
 
 func _ready() -> void:
     TimeManager.connect("time_updated", Callable(self, "_on_time_updated"))
 
 func _on_time_updated() -> void:
-    if TimeManager.get_current_day() == GameState.RENT_DUE_DAY:
+    if TimeManager.get_current_day() == GameState.RENT_DUE_DAY and !is_rent_collected:
         # Every month (on the day specified by RENT_DUE_DAY const)
         do_rent_collection_day()
+
+    elif TimeManager.get_current_day() == GameState.RENT_DUE_DAY + 1 and is_rent_collected:
+        is_rent_collected = false
 
     elif TimeManager.get_current_day() % 7 == 0 and GameState.current_scene == GameState.scenes_data.main_scene:
         go_to_convention()
 
 func go_to_convention() -> void:
+    TimeManager.force_pause_time()
     StatsManager.handle_task_action(GameState.PlayerTaskType.NONE)
     SceneManager.change_scene_to(SceneManager.convention_scene)
 
@@ -25,9 +30,9 @@ func do_rent_collection_day() -> void:
     if GameState.money >= GameState.rent_amount:
         StatsManager.spend_money(GameState.money)
         MessageLogManager.append_log("You paid $%d for your apartment")
+        is_rent_collected = true
         TimeManager.play_time()
     else:
         DialogueManager.show_dialogue_balloon(rent_due_dialogue, "cant_pay")
         await DialogueManager.dialogue_ended
-        StatsManager.spend_money(GameState.money)
         go_to_game_over()
