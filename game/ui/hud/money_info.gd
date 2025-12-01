5400b237e72ec6dd9042bf28900c13824c066e32
sabrina <sabriy2@uci.edu>
2025-12-01 04:05:56 -0800

    fix bug where money info didnt work in convention
    eeee

--- game/ui/hud/money_info.gd
+++ game/ui/hud/money_info.gd
@@ -4,8 +4,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    StatsManager.connect("money_updated", Callable(self, "_on_money_updated"))
+    StatsManager.connect("money_updated", _on_money_updated)
     amount_display.text = str(GameState.money)
 
-func _on_money_updated() -> void:
-    amount_display.text = str(GameState.money)
+func _on_money_updated(new_money: int, change: int) -> void:
+    amount_display.text = str(new_money)
